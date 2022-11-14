3626c3a55da857fd3df2de7250616d7f5a9da68e
Evgeny Bershdsky <79035534+jayber-dev@users.noreply.github.com>
2022-11-14 22:37:51 +0200

    project text and img added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,8 @@
 window.addEventListener('scroll', (e)=> {
-    console.log(location.href);
-    console.log(window.scrollY);
-    if(location.href !== 'http://127.0.0.1:5500/#main-section' || window.scrollY > 100) {
+    if(window.scrollY > 100) {
         document.querySelector('nav').style.display = 'none'
         document.querySelector('.side-menu-modal').style.display = 'block'
-    } else {
+    }  else {
         document.querySelector('nav').style.display = 'block'
         document.querySelector('.side-menu-modal').style.display = 'none'
     }
