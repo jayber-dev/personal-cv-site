795b1e1d741aaf9e802a98d7a51bae329e407567
Evgeny Bershdsky <79035534+jayber-dev@users.noreply.github.com>
2022-11-14 22:27:55 +0200

    project text and img added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,11 @@
-const imgData = document.querySelectorAll('.img-container');
-const rightSideContainer = document.querySelector('.right-side-img')
-imgData.forEach(element => {
-    element.addEventListener('mouseover', () => {
-       const img = element.querySelector('img');
-       console.log(img);
-       rightSideContainer.src = img.src
-    });
-    
-});
+window.addEventListener('scroll', (e)=> {
+    console.log(location.href);
+    console.log(window.scrollY);
+    if(location.href !== 'http://127.0.0.1:5500/#main-section' || window.scrollY > 100) {
+        document.querySelector('nav').style.display = 'none'
+        document.querySelector('.side-menu-modal').style.display = 'block'
+    } else {
+        document.querySelector('nav').style.display = 'block'
+        document.querySelector('.side-menu-modal').style.display = 'none'
+    }
+})
